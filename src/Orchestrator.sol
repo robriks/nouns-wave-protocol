ffe15d5e48271fed7453cb1185e098010efbf73c
robriks <marsterlund@gmail.com>
2024-01-11 17:47:07 -0500

    Fork mainnet to test basic Noun governance proposal

--- src/Orchestrator.sol
+++ src/Orchestrator.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {IDelegateRegistry} from "delegate-registry/src/IDelegateRegistry.sol";
+import {NounsDAOV3Proposals} from "nouns-monorepo/governance/NounsDAOV3Proposals.sol";
+import {NounsDAOLogicV3} from "nouns-monorepo/governance/NounsDAOLogicV3.sol";
+import {NounsDAOStorageV3} from "nouns-monorepo/governance/NounsDAOInterfaces.sol";
 
 contract Orchestrator {
     
@@ -10,7 +12,23 @@ contract Orchestrator {
     */
 
     address constant NOUNS_EXECUTOR = 0x0BC3807Ec262cB779b38D65b38158acC3bfedE10;
-    address constant NOUNS_GOVERNOR = payable(address(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d));
-    address constant NOUNS_TOKEN = 0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03;
-    address constant DELEGATE_CASH = 0x00000000000000447e69651d841bD8D104Bed493;
+    address payable public constant NOUNS_GOVERNOR = payable(address(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d));
+    address public constant NOUNS_TOKEN = 0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03;
+    address public constant DELEGATE_CASH = 0x00000000000000447e69651d841bD8D104Bed493;
+
+    function propose() public {
+        // NounsDAOStorageV3.StorageV3 storage ds;
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        string[] memory signatures = new string[](1);
+        bytes[] memory calldatas = new bytes[](1);
+        targets[0] = address(0x0);
+        values[0] = 1;
+        signatures[0] = '';
+        calldatas[0] = '';
+        string memory test = 'yes';
+        NounsDAOLogicV3(NOUNS_GOVERNOR).propose(targets, values, signatures, calldatas, test);
+    }
+
+    // todo: must inherit erc721receiver
 }
--- test/Orchestrator.t.sol
+++ test/Orchestrator.t.sol
@@ -2,8 +2,46 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
+import {NounsDAOProxyV3} from "nouns-monorepo/governance/NounsDAOProxyV3.sol";
+import {NounsDAOLogicV3} from "nouns-monorepo/governance/NounsDAOLogicV3.sol";
+import {NounsTokenLike} from "nouns-monorepo/governance/NounsDAOInterfaces.sol";
 import {Orchestrator} from "../src/Orchestrator.sol";
 
 contract OrchestratorTest is Test {
+    uint256 mainnetFork;
+    string MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
+
     Orchestrator public orchestrator;
+    NounsDAOLogicV3 public nounsGovernorProxy;
+    NounsTokenLike public nounsToken;
+
+    address someNounHolder; // random Nounder holding 17 Nouns at time of writing
+    uint256 someTokenId;
+    uint256 anotherTokenId;
+
+    function setUp() public {
+        mainnetFork = vm.createFork(MAINNET_RPC_URL);
+        vm.selectFork(mainnetFork);
+
+        orchestrator = new Orchestrator();
+        nounsGovernorProxy = NounsDAOLogicV3(orchestrator.NOUNS_GOVERNOR());
+        nounsToken = NounsTokenLike(orchestrator.NOUNS_TOKEN());
+
+        // pulled from etherscan for testing
+        someNounHolder = 0x13061efe742418c361C840CaFf300dC43AC0AffE;
+        someTokenId = 918;
+        anotherTokenId = 921;
+    }
+
+    function testPropose() public {
+        vm.startPrank(someNounHolder);
+        nounsToken.transferFrom(someNounHolder, address(orchestrator), someTokenId);
+        nounsToken.transferFrom(someNounHolder, address(orchestrator), anotherTokenId);
+        vm.stopPrank();
+        
+        // mine a block by rolling forward +1 to satisfy `getPriorVotes()` check 
+        vm.roll(block.number + 1);
+
+        orchestrator.propose(); 
+    }
 }
