e0cd58b662cd4febce75d28ac5ab6df41565f082
robriks <marsterlund@gmail.com>
2024-01-11 01:16:16 -0500

    Setup commit

---
+++ test/Orchestrator.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Orchestrator} from "../src/Orchestrator.sol";
+
+contract OrchestratorTest is Test {
+    Orchestrator public orchestrator;
+}
